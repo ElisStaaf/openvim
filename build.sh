d3900372b73d1b3145db62eabb9309587ab74fc5
eliss <elis.staaf@proton.me>
2024-12-13 08:10:03 +0100

    remove comment line

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 CFLAGS="-std=gnu2x -Os -s"
-# CFLAGS="-Og -g -fsanitize=undefined,address"
 
 for FILE in $(find . -name "*.c" -not -wholename "*/style/*" -not -wholename "*/bare_x86/*"); do
   if [ $FILE -nt ${FILE}.o ]; then
