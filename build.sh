96a2b2dae2c7f31122f6c65c5b7b08a1669d5850
eliss <elis.staaf@proton.me>
2024-12-25 00:40:26 +0100

    Fix a format issue
    So if I run this (for example):
        FILE="Hello.txt"
        cat $FILE >> ${FILE}.info
    I get this:
        cat Hello.txt >> Hello.txt.info
    *.txt.info isn't very pretty isn't it? Trim like this:
        ${FILE%.*}

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ CFLAGS="-std=gnu2x -Os -s"
 for FILE in $(find "./src" -name "*.c" -not -wholename "*/style/*" -not -wholename "*/bare_x86/*"); do
     if [ $FILE -nt ${FILE}.o ]; then
         echo $FILE
-        $CC -c $FILE -o ./${FILE}.o -I./include -Wall -Wextra $CFLAGS
+        $CC -c $FILE -o ./${FILE%.*}.o -I./include -Wall -Wextra $CFLAGS
     fi
 done
 
