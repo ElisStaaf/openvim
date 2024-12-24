5037c5c48aab7846c0405acf23fedfddfa54e84c
eliss <elis.staaf@proton.me>
2024-12-25 00:34:30 +0100

    Style fix

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
+CC=gcc
 CFLAGS="-std=gnu2x -Os -s"
 
 for FILE in $(find "./src" -name "*.c" -not -wholename "*/style/*" -not -wholename "*/bare_x86/*"); do
-  if [ $FILE -nt ${FILE}.o ]; then
-    echo $FILE
-    gcc -c $FILE -o ./${FILE}.o -Iinclude -Wall -Wextra $CFLAGS
-  fi
+    if [ $FILE -nt ${FILE}.o ]; then
+        echo $FILE
+        $CC -c $FILE -o ./${FILE}.o -I./include -Wall -Wextra $CFLAGS
+    fi
 done
 
-gcc $(find . -name "*.o") $CFLAGS -o ./openvim
+$CC $(find . -name "*.o") $CFLAGS -o ./openvim
